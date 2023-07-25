e111218cfc040f154b52c98a7e6f4004abe30319
Antonius Joshua <antoniusjoshua47@gmail.com>
2023-07-25 22:03:59 +0700

    feat: add feature job section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,41 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const featureJobs = [
+    {
+      title: "Admin Operation",
+      companyName: "PT Surya",
+      location: "Bekasi, Jawa Barat",
+      salary: "Rp. 5.000.000 - Rp. 10.000.000",
+      type: "Full Time",
+      date: "Posted 2 days ago",
+    },
+    {
+      title: "Product Manager",
+      companyName: "PT Indah",
+      location: "Jakarta, DKI Jakarta",
+      salary: "Rp. 10.000.000 - Rp. 15.000.000",
+      type: "Full Time",
+      date: "Posted 2 days ago",
+    },
+    {
+      title: "Software Engineer",
+      companyName: "PT Indopasifik Teknologi Medika Indonesia",
+      location: "Jakarta, DKI Jakarta",
+      salary: "Rp. 10.000.000 - Rp. 15.000.000",
+      type: "Full Time",
+      date: "Posted 2 days ago",
+    },
+    {
+      title: "Graphic Designer",
+      companyName: "PT Indopasifik Teknologi Medika Indonesia",
+      location: "Jakarta, DKI Jakarta",
+      salary: "Rp. 10.000.000 - Rp. 15.000.000",
+      type: "Full Time",
+      date: "Posted 2 days ago",
+    },
+  ];
+
   return (
     <>
       {/* Header Navbar */}
@@ -37,14 +72,70 @@ export default function Home() {
           <div className="flex flex-col gap-4">
             <h1 className="text-4xl font-bold">Cari Pekerjaan Impianmu</h1>
             <div className="flex gap-4">
-              <input type="text" placeholder="Cari Pekerjaan" />
-              <input type="text" placeholder="Lokasi" />
+              <input
+                type="text"
+                className="px-3 rounded-md"
+                placeholder="Cari Pekerjaan"
+              />
+              <input
+                type="text"
+                className="px-3 rounded-md"
+                placeholder="Lokasi"
+              />
+              <select className="px-4 text-gray-400 rounded-md">
+                <option value="1">Kategori</option>
+                <option value="2">Kategori 2</option>
+                <option value="3">Kategori 3</option>
+              </select>
               <button className="bg-white text-violet-900 px-3 py-2 rounded-md">
                 Cari
               </button>
             </div>
           </div>
         </form>
+      </section>
+
+      <section className="flex flex-col p-10">
+        <h1 className="font-bold text-4xl text-center text-uppercase text-violet-950">
+          Feature Jobs
+        </h1>
+
+        <section className="flex flex-row w-full mt-10 gap-4">
+          {/* Job Card */}
+          {featureJobs.map((job) => (
+            <div
+              className="flex w-1/4 border-2 border-blue-200 shadow-md rounded-md p-6 hover:cursor-pointer hover:shadow-xl"
+              key={job.title}
+            >
+              <div className="flex flex-col justify-between">
+                {/* Job Title */}
+                <div>
+                  <h2 className="text-xl font-bold text-violet-500">
+                    {job.title}
+                  </h2>
+                  {/* Company Name */}
+                  <h4 className="text-md">{job.companyName}</h4>
+                </div>
+
+                <div className="mt-5">
+                  {/* Job Location */}
+                  <h4 className="font-semibold">{job.location}</h4>
+                  {/* Job Salary */}
+                  <h4 className="font-semibold">{job.salary}</h4>
+                </div>
+
+                <div className="mt-5">
+                  {/* Job Type */}
+                  <button className="px-3 py-1 bg-[#E60378] text-white rounded-lg">
+                    {job.type}
+                  </button>
+                  {/* Job Posting Date */}
+                  <h4 className="text-gray-500 mt-5">{job.date}</h4>
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
       </section>
 
       {/* Job Categories */}
